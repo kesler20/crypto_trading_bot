406c4c123dab5ff3c29942e046c297b1a69e6e4f
kesler20 <uchekesla@gmail.com>
2022-07-29 15:23:07 +0100

    shhshs

--- data-api.py
+++ data-api.py
@@ -10,19 +10,17 @@ def calculate_sma(data: pd.DataFrame):
     col = data.columns
     result = []
     window = []
-    window_size = 20
+    window_size = 4
     for price in data[col[0]]:#
         window.append(price)
         if len(window) == window_size:
             result.append(sum(window)/window_size)
-            del window
+            window = []
     return result
 
 while True:
     data.append(get_live_price('TSLA'))
     time.sleep(WAIT_TIME)
     print(data)
-    # print(data)
-    # df = pd.DataFrame(data)
-    # df.to_csv('data.csv')
-    calculate_sma(data)
+    df = pd.DataFrame(data)
+    print(calculate_sma(df))
