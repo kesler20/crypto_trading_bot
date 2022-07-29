433472f2bc01dac223308427f1aadb830b7e1a3e
kesler20 <uchekesla@gmail.com>
2022-07-29 14:58:18 +0100

    thi

--- data_api.py
+++ data_api.py
@@ -1,3 +1,14 @@
 from yahoo_fin.stock_info import * 
+import matplotlib.pyplot as plt 
+import pandas as pd
+import time
 
-print(get_analysts_info('nflx'))
+data: list = []
+WAIT_TIME = 2 #seconds
+while True:
+    data.append(get_live_price('TSLA'))
+    time.sleep(WAIT_TIME)
+    print(data)
+    df = pd.DataFrame(data)
+    df.plot()
+    plt.show()
