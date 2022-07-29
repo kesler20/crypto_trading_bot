9c903deff41e5a56753a24114d037c724c97699f
kesler20 <uchekesla@gmail.com>
2022-07-29 15:19:15 +0100

    commond

--- data-api.py
+++ data-api.py
@@ -5,10 +5,24 @@ import time
 
 data: list = []
 WAIT_TIME = 2 #seconds
+
+def calculate_sma(data: pd.DataFrame):
+    col = data.columns
+    result = []
+    window = []
+    window_size = 20
+    for price in data[col[0]]:#
+        window.append(price)
+        if len(window) == window_size:
+            result.append(sum(window)/window_size)
+            del window
+    return result
+
 while True:
     data.append(get_live_price('TSLA'))
     time.sleep(WAIT_TIME)
     print(data)
-    df = pd.DataFrame(data)
-    df.plot()
-    plt.show()
+    # print(data)
+    # df = pd.DataFrame(data)
+    # df.to_csv('data.csv')
+    calculate_sma(data)
