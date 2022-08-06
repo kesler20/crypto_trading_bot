e75abd5b49eea2750a5536a022ffb7dc0acb191d
kesler20 <uchekesla@gmail.com>
2022-08-06 18:16:23 +0100

    improved the sma function

--- data-api.py
+++ data-api.py
@@ -6,21 +6,13 @@ import time
 data: list = []
 WAIT_TIME = 2 #seconds
 
-def calculate_sma(data: pd.DataFrame):
-    col = data.columns
-    result = []
-    window = []
-    window_size = 4
-    for price in data[col[0]]:#
-        window.append(price)
-        if len(window) == window_size:
-            result.append(sum(window)/window_size)
-            window = []
-    return result
+def calculate_sma(df: pd.DataFrame, i: int):
+    df[f'SMA{i}'] = df[0].rolling(i).mean()
+    return df
 
 while True:
     data.append(get_live_price('TSLA'))
     time.sleep(WAIT_TIME)
     print(data)
     df = pd.DataFrame(data)
-    print(calculate_sma(df))
+    print(calculate_sma(df,5))
